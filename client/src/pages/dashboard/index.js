cdd0b7b778ca98eb5c620b973b9961fb0d2d788a
KhjMeister <95680946+KhjMeister@users.noreply.github.com>
2023-02-09 20:58:27 +0330

    finished

---
+++ client/src/pages/dashboard/index.js
@@ -0,0 +1,8 @@
+import AddJob from './AddJob'
+import AllJobs from './AllJobs'
+import Profile from './Profile'
+import SharedLayout from './SharedLayout'
+import Stats from './Stats'
+import React from 'react'
+
+export { AllJobs, Profile, SharedLayout, Stats, AddJob }
---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import Landing from './Landing'
+import Error from './Error'
+import Register from './Register'
+import ProtectedRoute from './ProtectedRoute'
+import React from 'react'
+
+export { Landing, Error, Register, ProtectedRoute }
